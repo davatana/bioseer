14ae009ee43ff54b03252d80b899e7d388654b05
davatana <david.atanasov@mail.utoronto.ca>
2024-11-05 19:25:43 -0500

    Fixes styling

--- R/file_handler.R
+++ R/file_handler.R
@@ -3,12 +3,16 @@
 # Date: November 5 2024
 # Version: 0.0.1
 # Bugs and Issues: None as of November 5 2024
-
-#' List all CSV files in the data/ folder, which is where the activation data will be
+#'
+#' List all CSV files in the data/ folder, which is where the activation data 
+#' will be
 #' 
 #' @return A list of all the file names
 #' @export
 list_csv_files <- function() {
+
+  # Using the inbuilt R functions we filter for CSVs simply and return the 
+  # value
   file_list <- list.files(path = "data", pattern = "\\.csv$", full.names = TRUE)
   return(file_list)
 }
--- R/plot-activations.R
+++ R/plot-activations.R
@@ -3,7 +3,6 @@
 # Date: November 5 2024
 # Version: 0.0.1
 # Bugs and Issues: None as of November 5 2024
-Coding Style Guide 2024 5
 #' 
 #' @param file_path Path to the CSV file.
 #' @return A ggplot.
@@ -12,8 +11,11 @@ Coding Style Guide 2024 5
 plot_activations <- function(file_path) {
   library(ggplot2)
   
+  #We use the inbuilt R function to read the CSV but because of the way I've
+  #chosen to format the file we have to give the columns names here
   data <- read.csv(file_path, header = FALSE, col.names = c("AminoAcid", "Activation"))
   
+  # Summing up based on the second column which stores activations
   data_summary <- aggregate(Activation ~ AminoAcid, data, sum)
   
   ggplot(data_summary, aes(x = AminoAcid, y = Activation)) +
